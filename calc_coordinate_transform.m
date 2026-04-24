b56b0245a97dab9577b1d4f97fe8e92d84cde377
samvitkr <samvitkumar@gmail.com>
2026-04-24 12:39:55 -0500

    updates

--- calc_coordinate_transform.m
+++ calc_coordinate_transform.m
@@ -29,8 +29,12 @@ Nz=128;
 %wphi=reshape(wphi,Nx,1,Nz);
 c=2;
 me = [];
-phinl=zeros(Nx,Ny,Nz);
-for tstep=tstart:step:tend
+counter=0;
+
+ phinl=zeros(Nx,Ny,Nz);
+ nla = zeros(Nx,Ny,Nz);
+for tstep=tstart:tstart%step:tend
+    counter=counter+1;
     fn=sprintf('Sol%014d.h5',tstep);
     fnmat=sprintf('gradflux%014d.mat',tstep);
     fname = fullfile(baseDir,fn);
@@ -39,6 +43,9 @@ for tstep=tstart:step:tend
     fnmg = sprintf('grid%014d.mat',tstep);
     fng = fullfile(baseDir,fnmg);
     load(fng);
+    Jacobian=1./dZetadz;
+
+
     fnp=sprintf('potvel%014d.mat',tstep);
     fnp=fullfile(baseDir,fnp);
     load(fnp)
@@ -47,24 +54,27 @@ for tstep=tstart:step:tend
     uph=uphi./up;
     wph=wphi./up;
 
+    W=sqrt(up.*Jacobian);
+
     fprintf('Reading %s\n', fname);
     mt=matfile(fnamemat);
 
     u    = h5read(fname, '/u')-c;
     v    = h5read(fname, '/v');
     w=mt.wc;
-    ua = up.*(u.*uph + w.*wph);
-    ub = up.*v;
-    uc =up.*(-u.*wph + w.*uph);
-    
+    ua = W.*(u.*uph + w.*wph);
+    ub = W.*v;
+    uc = W.*(-u.*wph + w.*uph);
+
     %load(fnamemat)
     ox = mt.dwdy-mt.dvdz;
     oy = mt.dudz-mt.dwdx;
     oz = mt.dvdx-mt.dudy;
     clear mt;
-    oa = ox.*uph + oz.*wph;
-    ob = oy;
-    oc =-ox.*wph + oz.*uph;
+    oa = W.*(ox.*uph + oz.*wph);
+    ob = W.*oy;
+    oc = W.*(-ox.*wph + oz.*uph);
+    clear ox oy oz u v w
 
     fub=fft2(ub);
     fuc=fft2(uc);
@@ -72,15 +82,51 @@ for tstep=tstart:step:tend
     fob=fft2(ob);
     foc=fft2(oc);
 
-    phinl=phinl+(fub.*conj(foc)-fuc.*conj(fub))./(Nx*Ny);
+    phinl=phinl+(fub.*conj(foc)-fuc.*conj(fob))./(Nx*Ny);
 
-
-    % nla = (ub.*oc - uc.*ob);
-%     voz = single(v.*oz);
-%     woy = single(w.*oy);
-%     uoy = single((u).*oy);
-%     vox = single(v.*ox);
-%     nl = uphi.*(voz-woy)+wphi.*(uoy-vox);
-% errnl=nl-nla;
-% me = [me;max(abs(errnl),[],'all')];
+    % % % % Tphinl=squeeze(mean(phinl,[1 2]));
+    % % % %
+    % % % %
+    % % % % zz   = h5read(fname, '/zz');
+          nla = (ub.*oc - uc.*ob);
+    % % % %    JAnla = squeeze(mean(nla,2));
+    % % % %    JAnla(isnan(JAnla))=0;
+    % % % %
+    % % % %    % JAnla = trapz(zz',JAnla,2);
+    % % % %     % Tnla = trapz(X(:,1,1),JAnla);
+    % % % % Tnla = mean(JAnla,1);
+    % %  voz = single(v.*oz);
+    % %   woy = single(w.*oy);
+    % %   uoy = single((u).*oy);
+    % %   vox = single(v.*ox);
+    % %
+      % %nl = nl+uphi.*(voz-woy)+wphi.*(uoy-vox);
+    % % JAnl = squeeze(mean(nl,2));
+    % % JAnl(isnan(JAnl))=0;
+    % %
+    % % % JAnl = trapz(zz',Jacobian.*JAnl,2);
+    % % Tnl = trapz(X(:,1,1),Jacobian.*JAnl);
+    % errnl=nl-nla;
+    % me = [me;max(abs(errnl),[],'all')];
 end
+phinl=phinl./counter;
+
+%%
+% Tphinl=squeeze(mean(phinl,[1 2]));
+
+dX=X(3,3,3)-X(2,2,2);
+dY=Z(3,3,3)-Z(2,2,2);
+Tnla =squeeze( (dX*dY).*sum(nla,[1 2]));
+pex  = h5read(fname, '/pex');
+pey  = h5read(fname, '/pey');
+zz   = h5read(fname, '/zz');
+kx=pex*[0:Nx/2-1,0,-Nx/2+1:-1]';dkx=kx(2)-kx(1);
+ky=pey*[0:Ny/2-1,0,-Ny/2+1:-1]';dky=ky(2)-ky(1);
+
+Tnlphi= squeeze(real((dkx*dky).*sum(phinl./(Nx*Ny),[1 2])));
+%%
+figure
+hold on
+plot(Tnla,zz,'-b')
+plot(Tnlphi,zz,'-r')
+hold off
